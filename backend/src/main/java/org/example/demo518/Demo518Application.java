ec65adcf256bb9c600f3844095fe5395ca88b365
zz <123@qq.com>
2026-05-18 09:38:48 +0800

    环境1

---
+++ backend/src/main/java/org/example/demo518/Demo518Application.java
@@ -0,0 +1,13 @@
+package org.example.demo518;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo518Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo518Application.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/example/demo518/Demo518ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demo518;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo518ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
