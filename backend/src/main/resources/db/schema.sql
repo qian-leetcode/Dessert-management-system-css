c969f3587c7a1e2e7560b7db62e9d9e992660e25
egrajrofienogiewrg <3035005765@qq.com>
2026-05-18 15:10:09 +0800

    进程1

--- backend/src/main/resources/db/schema.sql
+++ backend/src/main/resources/db/schema.sql
@@ -1,10 +1,11 @@
 create database if not exists desserts;
+
 use desserts;
            # 甜品种类表
 create table if not exists category(
     id int auto_increment primary key,  -- 主键
     name varchar(100) not null, -- 种类名称
-description varchar(500) -- 甜品种类描述
+    description varchar(500) -- 甜品种类描述
 );
 
 # 甜品表
@@ -21,15 +22,16 @@ create table if not exists dessert(
 # 员工信息表
 CREATE TABLE if not exists t_user (
     id INT AUTO_INCREMENT PRIMARY KEY,    -- 员工编号  自增长
-    username VARCHAR(200) NOT NULL,       -- 登录用户名
-    password VARCHAR(200) NOT NULL,       -- 登录密码，经BCrypt算法加密
     name VARCHAR(100) NOT NULL,           -- 员工姓名
-    position VARCHAR(50),                 -- 岗位职位
-    active INT(1) DEFAULT 1,              -- 1用户可用，0 用户不可用
+    gender CHAR(1),                        -- 性别
     phone VARCHAR(20),                    -- 联系电话
+    username VARCHAR(200) NOT NULL,       -- 登录用户名
+    password VARCHAR(200) NOT NULL,       -- 登录密码，经BCrypt算法加密(不显示)
+    position VARCHAR(50),                 -- 岗位职位
+    active INT(1) DEFAULT 1,              -- 1用户可用，0 用户不可用(不显示 ，管理员显示并进行修改)
+                                        -- 可用为可编辑，不可用为不可编辑
     hire_date DATE,                       -- 入职日期
-    shift VARCHAR(20),                    -- 上班班次
-    gender CHAR(1)                        -- 性别
+    shift VARCHAR(20)                    -- 上班班次
     );
 
 # 职称信息表
@@ -45,3 +47,9 @@ create table if not exists t_user_role(
     user_id int references t_user(id),  # 引用用户
     role_id int references t_role(id)   # 引用角色
     );
+
+# 原材料信息表
+
+# 库存表
+
+# 采购信息表
---
+++ frontend/src/views/home.vue
@@ -0,0 +1,112 @@
+<script setup>
+import { ref } from 'vue'
+
+const regVisible = ref(false)
+
+const user_from_register = ref({
+  username : '',
+  password: '',
+  re_password: ''
+})
+
+const open_el_dialog = () =>{
+  regVisible.value = true
+}
+
+const push_from_register_data = () =>{
+  // console.log(user_from.value)
+  regVisible.value = false
+}
+
+</script>
+
+<template>
+    <el-container class="login-page">
+      <el-header class="login-header">
+        甜品管理系统
+      </el-header>
+      <el-main class="login-main">
+        <el-container style="font-size:32px">
+          账号登录
+        </el-container>
+        <el-container>
+          <el-input type="text" name="userName" placeholder="请输入用户名" class="input"/>
+        </el-container>
+        <el-container>
+          <el-input type="text" name="password" placeholder="请输入密码" class="input"/>
+        </el-container>
+        <el-container style="padding-top: 12px;justify-content: center; align-items: center">
+          <el-checkbox name="remember" /> 记住账号
+          <el-container style="justify-content: flex-end">
+          <span @click="open_el_dialog" class="clickable-text"> 没有账号？点击注册 </span>
+            <!-- 注册弹窗 -->
+            <el-dialog
+              :model-value="regVisible"
+              @update:model-value="regVisible = $event"
+              title="账号注册"
+              width="500px"
+
+            >
+              <el-form label-width="80px">
+                <el-form-item label="账号">
+                  <el-input v-model="user_from_register.username" />
+                </el-form-item>
+                <el-form-item label="密码">
+                  <el-input v-model="user_from_register.password" show-password/>
+                </el-form-item>
+                <el-form-item label="确认密码">
+                  <el-input v-model="user_from_register.re_password" show-password/>
+                </el-form-item>
+              </el-form>
+              <template #footer>
+                <el-button @click="regVisible = false"> 取消 </el-button>
+                <el-button type="primary" @click="push_from_register_data"> 立即注册 </el-button>
+              </template>
+
+            </el-dialog>
+
+          </el-container>
+        </el-container>
+        <el-button class="login-button">登录</el-button>
+      </el-main>
+      <el-footer >Copyright@2026</el-footer>
+    </el-container>
+</template>
+
+<style scoped>
+.login-page {
+  width: 100vw;
+  height: 100vh;
+  margin: 0 auto;
+  padding: 0;
+}
+
+.login-header {
+  background: rgb(150,200,255);
+  font-size: 72px;
+  height: 100px;
+}
+
+.login-main {
+  margin: 0 auto;
+  padding-top: 100px;
+}
+
+.input {
+  width: 450px;
+  height: 78px;
+  padding-top: 24px;
+}
+
+.clickable-text {
+  color: #409eff;           /* 蓝色，看起来像链接 */
+  cursor: pointer;          /* 鼠标变成手型 */
+  user-select: none;        /* 防止选中文字 */
+}
+
+.login-button {
+  width: 450px;
+  height: 48px;
+  background: #409eff;
+}
+</style>
